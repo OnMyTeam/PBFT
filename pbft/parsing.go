dbca90234dd61b2d513a8c702ae5efcfbf966f15
mckim <mincheolkim33@gmail.com>
2019-11-25 15:08:21 +0900

    add parsing.go

---
+++ pbft/parsing.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	    "os"
+	        "fmt"
+		    "bufio"
+		        "strings"
+		)
+
+		func main() {
+			    // 입력파일 열기
+			        fi, err := os.Open(os.Args[1])
+				    if err != nil {
+					            panic(err)
+						        }
+							    defer fi.Close()
+
+							        // 출력파일 생성
+								    fo, err := os.Create(os.Args[2])
+								        if err != nil {
+										        panic(err)
+											    }
+											        defer fo.Close()
+
+												            scanner:=bufio.NewScanner(fi)
+													                for scanner.Scan(){
+																                line:= scanner.Text()
+																		                line_split:=strings.Split(line,"[")
+																				                fmt.Println(len(line_split))
+																						                if(len(line_split)>1){
+																									                        line_split=strings.Split(line_split[1],"]")
+																												                        keyword:=line_split[0]
+																															                        fmt.Println(keyword)
+																																		                        if(keyword==os.Args[3]){
+																																						                                _,err:=fo.Write([]byte(line+"\n"))
+																																										                                if err!=nil{
+																																															                                        panic("파일 쓰기 실패")
+																																																				                                }
+																																																								                        }
+																																																											                }
+																																																													            }
+
+																																																													    }
